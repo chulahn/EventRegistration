15343e61d57b967446a73790df7d2db433ad0196
Chul Ahn <scahn.x@gmail.com>
2017-02-14 17:51:15 -0500

    ng-click
    able to vote on events

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -18,23 +18,36 @@ eventsApp.controller('EventController',
 					creatorName: "Bob Smith",
 					duration: "1 hr",
 					level: "Advanced",
-					abstract: "In this session you will learn the ins and outs of directives!"
+					abstract: "In this session you will learn the ins and outs of directives!",
+					upVoteCount: 0
 				},
 				{
 					name: "Scopes for fun and profit",
 					creatorName: "John Doe",
 					duration: "30 mins",
 					level: "Introductory",
-					abstract: "This session will take a closer look at scopes."
+					abstract: "This session will take a closer look at scopes.",
+					upVoteCount: 0
 				},
 				{
 					name: "Well Behaved Controllers",
 					creatorName: "Jane Doe",
 					duration: "2 hours",
 					level: "Intermediate",
-					abstract: "Controllers are the beginning of everything Angular does."
+					abstract: "Controllers are the beginning of everything Angular does.",
+					upVoteCount: 0
 				}
 			]
 		};
+
+
+		$scope.upVoteSession = function(session) {
+			session.upVoteCount++;
+		}
+
+		$scope.downVoteSession = function(session) {
+			session.upVoteCount--;
+		}
+
 	}
 );
